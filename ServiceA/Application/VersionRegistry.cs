b5d15f9c98af13e91bd057896aebd217c4634030
rtr-crmn <rtr.crmn.2@gmail.com>
2020-09-29 00:40:20 +0200

    feature-03 start (#4)
    Co-authored-by: user <user@localhost>

--- ServiceA/Application/VersionRegistry.cs
+++ ServiceA/Application/VersionRegistry.cs
@@ -5,7 +5,7 @@ namespace ServiceA.Application
     public static class VersionRegistry
     {
         
-        public static string Value => $"{nameof(ServiceA)} v1.2.0";
+        public static string Value => $"{nameof(ServiceA)} v1.3.0";
  
     }
 }
